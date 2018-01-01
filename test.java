97b31a2e9b406fe3ebe87d2dd9e14c0ec43978ed
Michelle Liu <michelleliu343@gmail.com>
2018-01-01 18:54:33 -0500

    add for loop

--- test.java
+++ test.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class test{
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = sc.next();
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+
+
+
+
     }
 }
